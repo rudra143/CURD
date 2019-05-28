3a4168ca295719d44bd7f7a566b091993b5fc697
Maharudra Kekan <kekanmaharudra71@gmail.com>
2019-05-28 14:49:39 +0530

    added indentation

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,94 +3,92 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-  public function index(){
-    $this->load->view('home', ['title'=>'Home']);
-
-  }
-
-  public function insert()
-  {
-
-    if ($this->form_validation->run('signup')) {
-        $data = $this->input->post(null, true);
-        unset($data['insert']);
-        $this->load->model('usermodel');
-        $this->usermodel->saveUser($data);
-        $output = array('status'=>'success','msg'=>'Record Saved successfully');
-        echo json_encode($output);
-        return null;
-    } else {
-      
-      	$output = array('status'=>'fail','error'=>validation_errors());
-      	echo json_encode($output);
-      	die();
-    }
-  }
-
-  public function fetchUsers(){
-	$this->load->model('usermodel');
-	$results = $this->usermodel->fetchUsers();
-	if ($results) {
-	// code...
-	$output = array('data'=> $results);
-	echo json_encode($output);
-	return null;
-	} else {
-	$output = array('status'=>'fail');
-	echo json_encode($output);
+	public function index(){
+		$this->load->view('home', ['title'=>'Home']);
 	}
-  }
 
-  public function updateUser() {
-      $id = $_POST['id'];
-      $this->load->model('usermodel');
-      $result = $this->usermodel->updateUser($id);
-      if ($result) {
-        $output = array('status'=>'success','result'=>$result);
-        echo json_encode($output);
-        return null;
-      } else {
-        $output = array('status'=>'fail');
-        echo json_encode($output);
-        return null;
-      }
-  }
+	public function insert()
+	{
 
-  public function updateUserData() {
-      $id = $_POST['id'];
-      $data = array(
-        'name' => $_POST['name'],
-        'email' => $_POST['email'],
-        'contact' => $_POST['contact']
-      );
-      $this->load->model('usermodel');
-      $result = $this->usermodel->update($id, $data);
-      if ($result) {
-        $output = array('status'=>'success','result'=>$result);
-        echo json_encode($output);
-        return null;
-      } else {
-        $output = array('status'=>'fail');
-        echo json_encode($output);
-        return null;
-      }
-  }
+		if ($this->form_validation->run('signup')) {
+			$data = $this->input->post(null, true);
+			unset($data['insert']);
+			$this->load->model('usermodel');
+			$this->usermodel->saveUser($data);
+			$output = array('status'=>'success','msg'=>'Record Saved successfully');
+			echo json_encode($output);
+			return null;
+		} else {
+			$output = array('status'=>'fail','error'=>validation_errors());
+			echo json_encode($output);
+			die();
+		}
+	}
 
-  public function delete()
-  {
-      $id = $_POST['id'];
-      $this->load->model('usermodel');
-      $result = $this->usermodel->deleteUser($id);
-      if ($result) {
-        $output = array('status'=>'success','result'=>$result);
-        echo json_encode($output);
-        return null;
-      } else {
-        $output = array('status'=>'fail');
-        echo json_encode($output);
-        return null;
-      }
-  }
+	public function fetchUsers(){
+		$this->load->model('usermodel');
+		$results = $this->usermodel->fetchUsers();
+		if ($results) {
+			// code...
+			$output = array('data'=> $results);
+			echo json_encode($output);
+			return null;
+		} else {
+			$output = array('status'=>'fail');
+			echo json_encode($output);
+		}
+	}
+
+	public function updateUser() {
+		$id = $_POST['id'];
+		$this->load->model('usermodel');
+		$result = $this->usermodel->updateUser($id);
+		if ($result) {
+			$output = array('status'=>'success','result'=>$result);
+			echo json_encode($output);
+			return null;
+		} else {
+			$output = array('status'=>'fail');
+			echo json_encode($output);
+			return null;
+		}
+	}
+
+	public function updateUserData() {
+		$id = $_POST['id'];
+		$data = array(
+			'name' => $_POST['name'],
+			'email' => $_POST['email'],
+			'contact' => $_POST['contact']
+		);
+		$this->load->model('usermodel');
+		$result = $this->usermodel->update($id, $data);
+		if ($result) {
+			$output = array('status'=>'success','result'=>$result);
+			echo json_encode($output);
+			return null;
+		} else {
+			$output = array('status'=>'fail');
+			echo json_encode($output);
+			return null;
+		}
+	}
+
+	public function delete()
+	{
+		$id = $_POST['id'];
+		$this->load->model('usermodel');
+		$result = $this->usermodel->deleteUser($id);
+		if ($result) {
+			$output = array('status'=>'success','result'=>$result);
+			echo json_encode($output);
+			return null;
+		} else {
+			$output = array('status'=>'fail');
+			echo json_encode($output);
+			return null;
+		}
+	}
 
 
 }
--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
       })
       .done(function(data){
         if (data.status == 'success') {
-          alert(data.msg);
-          $('#insertModal').modal('toggle');
-          $('#insertForm')[0].reset();
-          var err = `<span class="text text-danger">${data.error}</span>`;
-          $('input[type="text"][name="contact"]').append(err);
-          table.ajax.reload(); 
+            alert(data.msg);
+            $('#insertModal').modal('toggle');
+            $('#insertForm')[0].reset();
+            var err = `<span class="text text-danger">${data.error}</span>`;
+            $('input[type="text"][name="contact"]').append(err);
+            table.ajax.reload(); 
         }
       })
       .always(function(){
@@ -51,64 +51,64 @@ $(document).ready(function() {
   // });
 
 
-  var table = $('#infoTable').DataTable({
+var table = $('#infoTable').DataTable({
 
-    "ajax": 'http://localhost/CURD/home/fetchUsers',
-    "columns" : [
-      { "data": "name" },
-      { "data": "email" },
-      { "data": "contact" }
-    ],
-    "columnDefs": [
-      {
-        "targets": 3,
-        "data": null,
-        "defaultContent": "<button class='btn btn-success update' data-toggle='modal' data-target='#updateModal'><span class='fa fa-edit'></span></button>"
-      },
-      {
-        "targets": 4,
-        "data": null,
-        "defaultContent": "<button class='btn btn-danger delete'><span class='fa fa-trash'></span></button>"
-      }
-    ]
-  });
+	"ajax": 'http://localhost/CURD/home/fetchUsers',
+	"columns" : [
+		{ "data": "name" },
+		{ "data": "email" },
+		{ "data": "contact" }
+	],
+	"columnDefs": [
+		{
+			"targets": 3,
+			"data": null,
+			"defaultContent": "<button class='btn btn-success update' data-toggle='modal' data-target='#updateModal'><span class='fa fa-edit'></span></button>"
+		},
+		{
+			"targets": 4,
+			"data": null,
+			"defaultContent": "<button class='btn btn-danger delete'><span class='fa fa-trash'></span></button>"
+		}
+	]
+});
 
-  $('#infoTable tbody').on('click', 'button.update', function (e) {
-    e.preventDefault();
-    var data = table.row($(this).parents('tr')).data();
-    $('input[type="hidden"][name="id"]').val(data.id);
-    $('input[type="text"][name="name"]').val(data.name);
-    $('input[type="email"][name="email"]').val(data.email);
-    $('input[type="text"][name="contact"]').val(data.contact);
-  });
+$('#infoTable tbody').on('click', 'button.update', function (e) {
+	e.preventDefault();
+	var data = table.row($(this).parents('tr')).data();
+	$('input[type="hidden"][name="id"]').val(data.id);
+	$('input[type="text"][name="name"]').val(data.name);
+	$('input[type="email"][name="email"]').val(data.email);
+	$('input[type="text"][name="contact"]').val(data.contact);
+});
 
-  $('#updateForm').on('submit', function(e){
-    e.preventDefault();
+$('#updateForm').on('submit', function(e){
+	e.preventDefault();
 
-    $.ajax({
-      url: 'http://localhost/CURD/home/updateUserData',
-      type: 'POST',
-      dataType: 'json',
-      data: $('#updateForm').serialize()
-    })
-      .done(function (data) {
-        if (data.status == 'success') {
-          alert("Record Updated Successfully");
-          $('#updateModal').modal('toggle');
-          table.ajax.reload(); 
-        }
-      })
-      .always(function () {
-        console.log("complete");
-      });    
-  });
+	$.ajax({
+		url: 'http://localhost/CURD/home/updateUserData',
+		type: 'POST',
+		dataType: 'json',
+		data: $('#updateForm').serialize()
+	})
+	.done(function (data) {
+		if (data.status == 'success') {
+			alert("Record Updated Successfully");
+			$('#updateModal').modal('toggle');
+			table.ajax.reload(); 
+		}
+	})
+	.always(function () {
+		console.log("complete");
+	});    
+});
 
 
-  $('#infoTable tbody').on('click', 'button.delete', function (e) {
-    e.preventDefault();
-    var data = table.row($(this).parents('tr')).data();
+$('#infoTable tbody').on('click', 'button.delete', function (e) {
+	e.preventDefault();
+	var data = table.row($(this).parents('tr')).data();
 
-    if(confirm("Are you sure you want to delete this record??")){
+	if(confirm("Are you sure you want to delete this record??")){
 		$.ajax({
 			url: "http://localhost/CURD/home/delete",
 			type: 'POST',
@@ -122,10 +122,9 @@ $(document).ready(function() {
 			}
 		})
 		.always(function () {
-
 		});
 	}
-  });
+});
 
 
   // $.ajax({
