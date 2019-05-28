24dd2eccd412d2baf4b58662b66f0d060dbae9ac
Maharudra Kekan <kekanmaharudra71@gmail.com>
2019-05-28 15:10:16 +0530

    form_validation edit

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -1,33 +1,24 @@
 <?php
+	$config = [
 
+		'signup' => [
 
-
-        
-
-
-
-  $config = [
-  
-      'signup' => [
-  
-                [
-                  'field' => 'name',
-                  'label' => 'Name',
-                  'rules' => 'required|trim'
-                ],
-                [
-                  'field' => 'email',
-                  'label' => 'Email',
-                  'rules' => 'required|trim|Valid_email'
-                ],
-                [
-                  'field' => 'contact',
-                  'label' => 'Contact',
-                  'rules' => 'required|trim|max_length[10]|min_length[10]'
-                ]
-  
-      ],
-  
-  ];
+			[
+				'field' => 'name',
+				'label' => 'Name',
+				'rules' => 'required|trim'
+			],
+			[
+				'field' => 'email',
+				'label' => 'Email',
+				'rules' => 'required|trim|Valid_email'
+			],
+			[
+				'field' => 'contact',
+				'label' => 'Contact',
+				'rules' => 'required|trim|max_length[10]|min_length[10]'
+			]
+		],
+	];
 
  ?>
